610157bc90e9fa52cfa11b810e0bf91710278bbe
Aman <zbat37@gmail.com>
2025-05-22 23:09:01 +0530

    bug fix 3

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/My-portfolio-v2-/', // Replace with your GitHub repo name
+  base: '/My-portfolio/', // Replace with your GitHub repo name
 });
